cd49ecbe60866786d29cc2a5b76e15ec9edf0ed3
Karel Hala <khala@redhat.com>
2023-03-01 17:27:33 +0100

    Generate test file if export word found

--- extension.js
+++ extension.js
@@ -2,19 +2,20 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const COMMAND = 'auto-tests.generate';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
 /**
- * @param {vscode.ExtensionContext} context
+ * @param context
  */
 function activate(context) {
 
-  console.log(context, 'this is context!');
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auto-tests" is now active!');
+  context.subscriptions.push(
+		vscode.languages.registerCodeActionsProvider('javascript', new TestGenerator(), {
+			providedCodeActionKinds: TestGenerator.providedCodeActionKinds
+		}));
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -32,7 +33,58 @@ function activate(context) {
 // This method is called when your extension is deactivated
 function deactivate() {}
 
+/**
+ * Provides code actions for generating fild.
+ */
+class TestGenerator {
+  static providedCodeActionKinds = [
+		vscode.CodeActionKind.RefactorMove,
+    vscode.CodeActionKind.QuickFix,
+    // `${vscode.CodeActionKind.SourceFixAll}.autotests`
+	];
+
+  provideCodeActions(document, range, context, token){
+    if (!this.isAtStartOfExport(document, range)) {
+			return;
+		}
+    const folder = document.uri.path.replace(new RegExp(/[^\/]*\.*$/, 'i'), '');
+    const fileName = document.uri.path.substring(folder.length);
+    console.log(folder, fileName);
+    // console.log(document.uri, 'this is document!');
+    console.log(range, 'this is range!');
+    console.log(context, 'this is context!');
+    console.log(token, 'this is token!');
+    // if (context.only?.value.startsWith('source.fixAll')) {
+    // }
+    return [
+      this.createCommand(folder, fileName)
+    ]
+  }
+
+  isAtStartOfExport(document, range) {
+    const start = range.start;
+		const line = document.lineAt(start.line);
+    return line.text.startsWith('export');
+  }
+
+  createCommand(folder, fileName) {
+    const extension = fileName.replace(new RegExp(/[^\.]*/, 'i'), '');
+    const symbolName = fileName.substring(0, fileName.length - extension.length);
+    console.log(symbolName, extension, fileName);
+    const action = new vscode.CodeAction('Generate test file', vscode.CodeActionKind.RefactorMove);
+    action.edit = new vscode.WorkspaceEdit();
+    const config = vscode.workspace.getConfiguration('autotests');
+    const newFileName = config.filename.replace('[filename]', symbolName);
+    const filePath = vscode.Uri.file(`${folder}/${newFileName}`);
+    action.edit.createFile(filePath, { ignoreIfExists: true });
+    // vscode.window.showInformationMessage('Created a new file: hello/world.md');
+    action.isPreferred = true;
+		return action;
+  }
+}
+
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate,
+  TestGenerator
 }
