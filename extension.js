18a15f646988ae2716d5d2d62eaddc527fa1169c
Karel Hala <khala@redhat.com>
2023-03-01 10:48:53 +0100

    Add extension config

--- extension.js
+++ extension.js
@@ -10,6 +10,8 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
+  console.log(context, 'this is context!');
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "auto-tests" is now active!');
@@ -17,7 +19,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-tests.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('auto-tests.generate', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
