34b8262583ff03207a97b4c223fe098582cb986b
Karel Hala <khala@redhat.com>
2023-03-02 18:01:30 +0100

    Generate new test file and insert imports and describes

--- extension.js
+++ extension.js
@@ -2,10 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-const COMMAND = 'auto-tests.generate';
+function getNames(document, range) {
+  const start = range.start;
+  const line = document.lineAt(start.line);
+  const name = line.text.match(new RegExp(/[\w\d]+(?=\(|\s+=|\s+\()/, 'i'))?.[0];
+  if (name) {
+    return [name];
+  }
+  // export { foo, bar, baz } from 'foo';
+  return line.text.match(new RegExp(/([\d\w])+(?=\s*,|\s*\})/, 'ig'));
+}
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function getFileInfo(document) {
+  const folder = document.uri.path.replace(new RegExp(/[^\/]*\.*$/, 'i'), '');
+  const fileName = document.uri.path.substring(folder.length);
+  const extension = fileName.replace(new RegExp(/[^\.]*/, 'i'), '');
+  const symbolName = fileName.substring(0, fileName.length - extension.length);
+  return {folder, fileName, extension, symbolName};
+}
+
+function newInserts(names, symbolName) {
+  const newImports = `import { ${names?.join(', ')} } from './${symbolName}';\n`;
+  const newDescribes = names?.map((name) => `describe('${name}', () => {
+    test('dummy test', () => {
+      expect(${name}).toBeDefined();
+    });
+});`)?.join('\n');
+  return [newImports, newDescribes];
+}
 
 /**
  * @param context
@@ -17,17 +41,28 @@ function activate(context) {
 			providedCodeActionKinds: TestGenerator.providedCodeActionKinds
 		}));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-tests.generate', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from auto-tests!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('auto-tests.generate', async function () {
+    const config = vscode.workspace.getConfiguration('autotests');
+    const editor = vscode.window.activeTextEditor;
+    const names = getNames(editor.document, editor.selection);
+    const { symbolName, folder, fileName } = getFileInfo(editor.document);
+    const newFileName = config.filename.replace('[filename]', symbolName);
+    const filePath = vscode.Uri.file(`${folder}${newFileName}`);
+    const workEdits = new vscode.WorkspaceEdit();
+    workEdits.createFile(filePath, { ignoreIfExists: true });
+    await vscode.workspace.applyEdit(workEdits);
+    const doc = await vscode.workspace.openTextDocument(filePath);
+    const [newImports, newDescribes] = newInserts(names, symbolName);
+    await vscode.window.showTextDocument(doc);
+    const newTestFile = vscode.window.activeTextEditor;
+    const lastPos = newTestFile.document.positionAt(newTestFile.document.lineCount);
+    const firstPos = newTestFile.document.positionAt(1);
+    console.log(lastPos, firstPos);
+    newTestFile.edit(builder => {
+      builder.replace(firstPos, newImports);
+      builder.replace(lastPos, newDescribes);
+    })
+	}));
 }
 
 // This method is called when your extension is deactivated
@@ -47,17 +82,15 @@ class TestGenerator {
     if (!this.isAtStartOfExport(document, range)) {
 			return;
 		}
+
+    const exportedNames = getNames(document, range);
     const folder = document.uri.path.replace(new RegExp(/[^\/]*\.*$/, 'i'), '');
     const fileName = document.uri.path.substring(folder.length);
     console.log(folder, fileName);
-    // console.log(document.uri, 'this is document!');
-    console.log(range, 'this is range!');
-    console.log(context, 'this is context!');
-    console.log(token, 'this is token!');
     // if (context.only?.value.startsWith('source.fixAll')) {
     // }
     return [
-      this.createCommand(folder, fileName)
+      this.createCommand(folder, fileName, exportedNames)
     ]
   }
 
@@ -67,17 +100,16 @@ class TestGenerator {
     return line.text.startsWith('export');
   }
 
-  createCommand(folder, fileName) {
-    const extension = fileName.replace(new RegExp(/[^\.]*/, 'i'), '');
-    const symbolName = fileName.substring(0, fileName.length - extension.length);
-    console.log(symbolName, extension, fileName);
+  createCommand(folder, fileName, exportedNames) {
+    // const extension = fileName.replace(new RegExp(/[^\.]*/, 'i'), '');
+    // const symbolName = fileName.substring(0, fileName.length - extension.length);
     const action = new vscode.CodeAction('Generate test file', vscode.CodeActionKind.RefactorMove);
-    action.edit = new vscode.WorkspaceEdit();
-    const config = vscode.workspace.getConfiguration('autotests');
-    const newFileName = config.filename.replace('[filename]', symbolName);
-    const filePath = vscode.Uri.file(`${folder}/${newFileName}`);
-    action.edit.createFile(filePath, { ignoreIfExists: true });
-    // vscode.window.showInformationMessage('Created a new file: hello/world.md');
+    // action.edit = new vscode.WorkspaceEdit();
+    // const config = vscode.workspace.getConfiguration('autotests');
+    // const newFileName = config.filename.replace('[filename]', symbolName);
+    // const filePath = vscode.Uri.file(`${folder}${newFileName}`);
+    // action.edit.createFile(filePath, { ignoreIfExists: true });
+    // action.command = { command: 'auto-tests.generate-functions', title: 'Learn more about emojis', tooltip: 'This will open the unicode emoji page.' };
     action.isPreferred = true;
 		return action;
   }
